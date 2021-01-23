5fc176b8a10528e326f6c97cfd05b65729e3a1fd
Aayush Chadha <aayush.chadha@student.manchester.ac.uk>
2021-01-23 17:03:03 +0000

    Add news card to feed

---
+++ src/components/Feed.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div v-if="feedElements">
+      <md-card v-if="currentCardType === 'news_headline'">
+        <md-card-header>
+          <md-avatar>
+            <!-- TODO add  news outlet logo-->
+<!--            <img src={{currentCard.source_logo}} alt="Avatar">-->
+          </md-avatar>
+
+          <div class="md-title">{{feedElements[currentCard].headline}}</div>
+          <div class="md-subhead">{{feedElements[currentCard].friends_interactions}}</div>
+        </md-card-header>
+
+        <md-card-content>
+          {{ feedElements[currentCard].news_summary}}
+          <h3 class="md-subheading">Where do you think this article lies on the political spectrum?</h3>
+          <div class="rating-spectrum">
+            <md-radio v-model="rating" value="sLeft">Strongly Left</md-radio>
+            <md-radio v-model="rating" value="Left">Left</md-radio>
+            <md-radio v-model="rating" value="Centre">Centre</md-radio>
+            <md-radio v-model="rating" value="Right">Right</md-radio>
+            <md-radio v-model="rating" value="sRight">Strongly Right</md-radio>
+          </div>
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button v-on:click="submitRating">Submit</md-button>
+        </md-card-actions>
+      </md-card>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .md-card {
+    border-radius: 4px;
+    width: 320px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+}
+</style>
+
+<script>
+  export default {
+    name: "Feed",
+    data: () => ({
+      feedElements: null,
+      currentCardType: null,
+      currentCard: 0,
+      reachedEndOfFeed: false,
+      rating: null,
+      userInteractionEvents: []
+    }),
+
+    methods: {
+      submitRating: () => {
+        let event = {
+          "user_id": "User 1",
+          "item_id": this.feedElements[this.currentCard].id,
+          "rating": this.rating
+        }
+        this.userInteractionEvents.push(event);
+        let nextCard = this.currentCard + 1;
+
+        if (nextCard !== length(this.feedElements)) {
+          this.currentCard = nextCard;
+          this.currentCardType = this.feedElements[nextCard].type;
+        }
+
+        else {
+          this.reachedEndOfFeed = true;
+          <!-- TODO send interaction events to database-->
+        }
+      }
+    }
+  }
+</script>
