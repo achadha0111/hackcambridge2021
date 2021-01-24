9c9beb21b809f5070b3a80ad4d5b9682cc6a04e1
Aayush Chadha <aayush.chadha@student.manchester.ac.uk>
2021-01-24 11:46:05 +0000

    Fix card carousel

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,82 +1,103 @@
 <template>
   <div>
-    <div v-if="feedElements">
+    <div class="md-layout">
+      <div class="md-layout-item"></div>
+      <div class="md-layout-item md-size-50">
+        <div :key="currentCardIndex" v-if="currentCard" >
 
-      <!-- News card -->
-      <md-card v-if="currentCardType === 'P'">
-        <md-card-header>
-          <div class="md-title">{{feedElements[currentCard]["headline"]}}</div>
-<!--          <div class="md-subhead">{{feedElements[currentCard].friends_interactions}}</div>-->
-        </md-card-header>
+          <!-- News card -->
+          <md-card v-if="currentCardType === 'P'">
+            <md-card-header>
+              <div class="md-title">{{ currentCard["opheadline"] }}</div>
+              <!--          <div class="md-subhead">{{feedElements[currentCard].friends_interactions}}</div>-->
+            </md-card-header>
 
-        <md-card-content>
-<!--          <a :href = feedElements[currentCard]["opinion_source"]>{{feedElements[currentCard]["opinion_source"]}}</a>-->
-          <h3 class="md-subheading">Where do you think this article lies on the political spectrum?</h3>
-          <div class="rating-spectrum">
-            <md-radio v-model="rating" value="sLeft">Strongly Left</md-radio>
-            <md-radio v-model="rating" value="Left">Left</md-radio>
-            <md-radio v-model="rating" value="Centre">Centre</md-radio>
-            <md-radio v-model="rating" value="Right">Right</md-radio>
-            <md-radio v-model="rating" value="sRight">Strongly Right</md-radio>
+            <md-card-content>
+              <a :href = "currentCard['opinion_source']">{{ currentCard["opinion_source"] }}</a>
+              <h3 class="md-subheading">Where do you think this article lies on the political spectrum?</h3>
+              <div class="rating-spectrum">
+                <md-radio v-model="rating" value="sLeft">Strongly Left</md-radio>
+                <md-radio v-model="rating" value="Left">Left</md-radio>
+                <md-radio v-model="rating" value="Centre">Centre</md-radio>
+                <md-radio v-model="rating" value="Right">Right</md-radio>
+                <md-radio v-model="rating" value="sRight">Strongly Right</md-radio>
+              </div>
+            </md-card-content>
+
+            <md-button v-on:click="submitRating">Submit</md-button>
+
+          </md-card>
+          <!-- News card -->
+          <!-- Video -->
+          <div v-else>
+            <md-card >
+              <md-card-header>
+                <div v-if="currentCardType === 'V'"
+                     class="md-title">Is this video fake?</div>
+                <div v-else>
+                  <div  class="md-title"> Is this a fake headline?</div>
+                  <h3>{{ currentCard["fheadline"] }}</h3>
+                  <h4>Outlet: <span>{{ currentCard["newsoutlet"] }} </span></h4>
+                </div>
+
+                <div class="md-subhead">Select Fake/Not Fake to proceed</div>
+              </md-card-header>
+
+              <md-card-media md-big>
+                <!-- Todo add video link -->
+                <!--          <img src="/assets/examples/avatar-2.jpg" alt="People">-->
+
+                <div v-if="currentCardType === 'V'">
+                  <youtube-media player-height="640" player-width="450" v-if="currentCardType === 'V'"
+                                 :video-id="currentCard['video_id']"></youtube-media>
+                </div>
+              </md-card-media>
+
+              <md-card-content>
+                <!--          <div v-if="currentCardType === 'fake_headline'">-->
+                <!--            <h2>{{feedElements[currentCard].headline}}</h2>-->
+                <!--          </div>-->
+
+                <div class="rating-spectrum">
+                  <md-radio v-model="rating" :value=1>Fake</md-radio>
+                  <md-radio v-model="rating" :value=0>Not fake</md-radio>
+                </div>
+              </md-card-content>
+
+
+              <md-button v-on:click="submitRating" class="md-raised md-accent">Submit</md-button>
+
+            </md-card>
+            <!-- Video // Fake Headline -->
+
           </div>
-        </md-card-content>
 
-        <md-card-actions>
-          <md-button v-on:click="submitRating">Submit</md-button>
-        </md-card-actions>
-      </md-card>
-      <!-- News card -->
 
-      <!-- Video -->
-      <md-card v-if="currentCardType === 'V' || currentCardType === 'FH'">
-        <md-card-header>
-          <div v-if="currentCardType === 'V'"
-               class="md-title">Is this video fake?</div>
-          <div v-else class="md-title"> Is this a misleading headline?</div>
-          <div class="md-subhead">Answer yes or no to proceed</div>
-        </md-card-header>
+        </div>
+        <div v-else>You have reached the end of your feed, check back later :)</div>
 
-        <md-card-media>
-          <!-- Todo add video link -->
-<!--          <img src="/assets/examples/avatar-2.jpg" alt="People">-->
-          <youtube-media v-if="currentCardType === 'V'"
-                         :video-id="feedElements[currentCard].id"></youtube-media>
-        </md-card-media>
-
-        <md-card-content>
-<!--          <div v-if="currentCardType === 'fake_headline'">-->
-<!--            <h2>{{feedElements[currentCard].headline}}</h2>-->
-<!--          </div>-->
-
-          <div class="rating-spectrum">
-            <md-radio v-model="rating" :value=1>Fake</md-radio>
-            <md-radio v-model="rating" :value=0>Not fake</md-radio>
-          </div>
-        </md-card-content>
-
-        <md-card-actions>
-          <md-button v-on:click="submitRating">Submit</md-button>
-        </md-card-actions>
-      </md-card>
-      <!-- Video // Fake Headline -->
-
+      </div>
+      <div class="md-layout-item"></div>
+      </div>
     </div>
-    <div v-else>You have reached the end of your feed, check back later :)</div>
-  </div>
+
 </template>
 
 <style lang="scss" scoped>
   .md-card {
     border-radius: 4px;
-    width: 320px;
-    margin: 4px;
-    display: inline-block;
-    vertical-align: top;
-}
+    padding: 0 !important;
+  }
+
+  md-card-media {
+    height: 75% !important;
+  }
+
 </style>
 
 <script>
 import axios from 'axios';
+import {getIdFromURL} from 'vue-youtube-embed';
 
 async function fetchFeedElements(callback) {
     try {
@@ -93,9 +114,11 @@ async function fetchFeedElements(callback) {
     data: () => ({
       feedElements: null,
       currentCardType: null,
-      currentCard: 0,
+      currentCardIndex: 0,
+      currentCard: null,
       reachedEndOfFeed: false,
       rating: null,
+      cardUpdated: 'no',
       userInteractionEvents: [],
       error: null,
     }),
@@ -110,22 +133,26 @@ async function fetchFeedElements(callback) {
     },
 
     methods: {
-      submitRating: () => {
-        let event = {
-          "user_id": "User 1",
-          "item_id": this.feedElements[this.currentCard].id,
-          "rating": this.rating
-        }
-        this.userInteractionEvents.push(event);
-        let nextCard = this.currentCard + 1;
-
-        if (nextCard !== length(this.feedElements)-1) {
-          this.currentCard = nextCard;
+      submitRating: function () {
+        // let event = {
+        //   "user_id": "User 1",
+        //   "item_id": this.feedElements[this.currentCardIndex].id,
+        //   "rating": this.rating
+        // }
+        //
+        // this.userInteractionEvents.push(event);
+        let nextCard = this.currentCardIndex + 1;
+        if (nextCard !== (this.feedElements).length) {
+          this.currentCard = this.feedElements[nextCard];
           this.currentCardType = this.feedElements[nextCard].type;
+          this.currentCardIndex = this.currentCardIndex+1;
+
+
         }
 
         else {
           this.reachedEndOfFeed = true;
+          this.currentCard = null;
           // TODO send interaction events to database
         }
       },
@@ -136,9 +163,13 @@ async function fetchFeedElements(callback) {
           console.log(this.error);
         } else {
           this.feedElements = feedElements.data;
-          this.currentCardType = this.feedElements[this.currentCard].CARDTYPE;
-          console.log(typeof this.feedElements);
-          console.log(this.feedElements[0]);
+          this.currentCardType = this.feedElements[this.currentCardIndex].CARDTYPE;
+          this.currentCard = this.feedElements[this.currentCardIndex]
+          this.feedElements.forEach((card, index) => {
+            if (card["CARDTYPE"] === 'V') {
+              this.feedElements[index].video_id = getIdFromURL(card["sourcelink"])
+            }
+          })
         }
       }
     }
